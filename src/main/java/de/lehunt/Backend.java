c774dcf7c78503f3ecf35f441f5ac172051f5a7b
Sebastian Bilda <sebastian.bilda@outlook.de>
2020-01-10 19:59:09 +0100

    Backend init

--- src/main/java/de/lehunt/Backend.java
+++ src/main/java/de/lehunt/Backend.java
@@ -1,87 +1,167 @@
 package de.lehunt;
 
-import com.hivemq.client.mqtt.datatypes.MqttQos;
-import com.hivemq.client.mqtt.mqtt3.Mqtt3AsyncClient;
-import com.hivemq.client.mqtt.mqtt3.Mqtt3Client;
-import com.hivemq.client.mqtt.mqtt3.message.connect.connack.Mqtt3ConnAck;
-import com.hivemq.client.mqtt.mqtt3.message.publish.Mqtt3Publish;
+import org.eclipse.paho.client.mqttv3.*;
+import org.json.JSONObject;
 
-import java.util.UUID;
-import java.util.concurrent.ExecutionException;
-import java.util.function.Consumer;
 
 public class Backend {
 
-    private static final String TOPIC_FILTER = "test/topic";
-    private static final  String HOST = "broker.hivemq.com";
+    private static final  String BROKER = "tcp://192.168.2.184:1883";
+    private static final int QOS_EXACTLY_ONCE = 1;
+    private final String  clientId = "BackendClient";
+    private MqttClient mqttClient;
     private final HintLookupCallback hintLookupCallback;
 
-    Backend(HintLookupCallback hintLookupCallback) {
+
+    public Backend(HintLookupCallback hintLookupCallback){
         this.hintLookupCallback = hintLookupCallback;
     }
 
-    public static void main(String[] args) {
-        Backend backend = new Backend(new StaticHintLookupCallback());
-        backend.start();
-    }
-
     public void start() {
-        Mqtt3AsyncClient client = Mqtt3Client.builder()
-                .identifier(UUID.randomUUID().toString())
-                .serverHost(HOST)
-                .buildAsync();
-        try {
-            Mqtt3ConnAck mqtt3ConnAck = client.connect().get();
-            if (mqtt3ConnAck.getReturnCode().isError()) {
-                System.out.println("Backend could not connect to MQTT Broker with address " + HOST);
-            } else {
-                System.out.println("Backend conncted successfully to MQTT Broker with address " + HOST);
-            }
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
 
-        OnMessageCallback onMessageCallback = new OnMessageCallback(client, hintLookupCallback);
+        // Create Mqtt Client and Connect to Broker
 
-        client.subscribeWith()
-                .topicFilter(TOPIC_FILTER) //TODO adjust the topic filter
-                .qos(MqttQos.EXACTLY_ONCE)
-                .callback(onMessageCallback)
-                .send();
+
+		try {
+            mqttClient = new MqttClient(BROKER, clientId);
+
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            System.out.println("Connecting to broker: "+ BROKER);
+            mqttClient.connect();
+            System.out.println("Connected");
+
+            OnMessageCallback callback = new OnMessageCallback(mqttClient, hintLookupCallback);
+		    // mqttClient.setCallback(callback);
+
+		    mqttClient.subscribe("#", QOS_EXACTLY_ONCE, callback);
+            System.out.println("Subscribed to all Topics");
+
+		} catch (MqttException e) {
+            System.out.println("Backend could not connect to MQTT Broker with address " + BROKER);
+			// e.printStackTrace();
+		};
     }
 
-    private static class OnMessageCallback implements Consumer<Mqtt3Publish> {
-        private final Mqtt3AsyncClient client;
+    private static class OnMessageCallback implements IMqttMessageListener {
+
+        private final MqttClient client;
         private final HintLookupCallback hintLookupCallback;
 
-        private OnMessageCallback(Mqtt3AsyncClient client, HintLookupCallback hintLookupCallback) {
-            this.client = client;
-            this.hintLookupCallback = hintLookupCallback;
+        private OnMessageCallback(MqttClient c, HintLookupCallback hlc){
+            this.client = c;
+            this.hintLookupCallback = hlc;
         }
 
+
         @Override
-        public void accept(Mqtt3Publish mqtt3Publish) {
-            String topic = mqtt3Publish.getTopic().toString();
-            //extract the id of the Hint
+        public void messageArrived(String topic, MqttMessage message) throws Exception {
 
             String[] splittedTopic = topic.split("/");
 
 
             String clientId = splittedTopic[1];
             String huntId = splittedTopic[0];
-            String json = new String(mqtt3Publish.getPayloadAsBytes());
-            //TODO extract from topic
-            System.out.println("Message arrived on topic: '" + topic + "' with payload: '" + new String(mqtt3Publish.getPayloadAsBytes()) + "'");
-            //lookup the Hint
-            String hint = hintLookupCallback.lookupHint(json);
-            //send out the hint
-            String responseTopic = huntId + "/" + clientId;
+            String json = null;
+            try {
+                json = new String(message.getPayload());
+                System.out.println("Message arrived on topic: '" + topic + "' with payload: '" + new String(message.getPayload()) + "'");
+                //lookup the Hint
+                String hint = hintLookupCallback.lookupHint(json);
+                //send out the hint
+                String responseTopic = huntId + "/" + clientId;
 
-            System.out.println("Sending response on topic '" + responseTopic + "'" + " with payload '" + hint + "'.");
-            client.publishWith()
-                    .topic(responseTopic)
-                    .payload(hint.getBytes())
-                    .send();
+                System.out.println("Sending response on topic '" + responseTopic + "'" + " with payload '" + hint + "'.");
+                client.publish(responseTopic, new MqttMessage(hint.getBytes()));
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+            //TODO extract from topic
+
         }
+
+    }
+
+    /*private MqttCallback OnMessageCallback = new MqttCallback() {
+
+        @Override
+        public void connectionLost(Throwable cause) {
+
+        }
+
+        @Override
+        public void messageArrived(String topic, MqttMessage message) throws Exception {
+            System.out.println(new String(message.getPayload()));
+            // LookupNewHint(new String(message.getPayload()), topic);
+
+        }
+
+        @Override
+        public void deliveryComplete(IMqttDeliveryToken token) {
+
+        }
+    };*/
+
+    private void LookupNewHint(String payload, String topic){
+        JSONObject obj = new JSONObject(payload);
+
+        String splittedTopic[] = topic.split("/");
+
+        String huntid= splittedTopic[0];
+        String clientid = splittedTopic[1];
+
+        String pubTopic = huntid + "/" + clientid + "/down";
+        String returnMessage = "ihr neuer hinweis ist: bla bla bla";
+
+        // TODO: 10.01.2020 split json object
+        // TODO: 10.01.2020 lookup new hint
+
+        System.out.println(obj.getString("advertisment"));
+
+        try {
+            new MqttClient(BROKER, "PubClient").publish(pubTopic, new MqttMessage(returnMessage.getBytes()));
+        } catch (MqttException e) {
+            System.out.println("Error: Publish message failed");
+            e.printStackTrace();
+        }
+    }
+
+    public interface HintLookupCallback {
+
+        String lookupHint(String identifier);
+
+    }
+
+    public static class StaticHintLookupCallback implements HintLookupCallback {
+
+        @Override
+        public String lookupHint(String json) {
+
+
+            String hintId = extractHuntIDFromJson(json);
+            switch (hintId) {
+                case "1":
+                    return "Insert ur first hint here";
+                case "2":
+                    return "Another hint";
+                // TODO Insert more hints here...
+                default:
+                    return "Hint was not found on backend.";
+            }
+        }
+
+        private String extractHuntIDFromJson(String json) {
+
+            return "1"; // TODO extract from json
+
+
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        Backend backend = new Backend(new StaticHintLookupCallback());
+        backend.start();
     }
 }
--- src/main/java/de/lehunt/HintLookupCallback.java
+++
@@ -1,8 +0,0 @@
-package de.lehunt;
-
-
-public interface HintLookupCallback {
-
-    String lookupHint(String identifier);
-
-}
